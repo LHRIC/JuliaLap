019bedb86865da0a47fa600fa3793f9035a15401
SirCrocs <royliu2007@gmail.com>
2025-09-19 17:44:55 -0500

    Explanations on function uses are in the main function

--- DampeningSpringsCode.jl
+++ DampeningSpringsCode.jl
@@ -1,18 +1,9 @@
-import Pkg;
-Pkg.add("CSV")
-Pkg.add("DataFrames")
-Pkg.add("GLMakie")
-Pkg.add("Interpolations")
 using CSV
 using DataFrames
 using GLMakie
 using Interpolations
 
-function main()
-
-    #Get file path
-    print("Enter CSV file path: ")
-    file_path = strip(readline())
+function get_settings(file_path::AbstractString) #AbstractString works for both substrings and strings
 
     #Check if file for low or high speed
     file_type = (occursin("_LS", file_path) || occursin(" LS", file_path)) ? "LS" : "HS"
@@ -43,25 +34,37 @@ function main()
     end
 
     available_settings = sort(collect(setting_values)) #collect(set) converts set to array, sorted in ascending order
-    println("Available settings: ", available_settings)
+    return available_settings
+end
 
-    #Get user input
-    print("Enter desired setting: ")
-    desired_setting = parse(Float64, readline())
+function get_interpolation(file_path::AbstractString, desired_setting::Float64)
 
-    fig = Figure(size = (1000, 700)) #Inches to pixels conversion using 100 dpi
-    ax = Axis(fig[1, 1], xlabel="Velocity (mm/sec)", ylabel="Force (N)")
+    #Check if file for low or high speed
+    file_type = (occursin("_LS", file_path) || occursin(" LS", file_path)) ? "LS" : "HS"
+
+    #Load file into dataframe
+    df = nothing #Needed for scope
+    try
+        df = CSV.read(file_path, DataFrame)
+    catch e
+        println("Error loading file; ", e)
+        return
+    end
+
+    available_settings = get_settings(file_path)
 
     if desired_setting in available_settings
         if file_type == "LS"
             setting_str = (isapprox(desired_setting, round(desired_setting)) ? 
-           string(Int(round(desired_setting))) * "-4.3" : string(Int(round(desired_setting))) * "-4.3" : string(desired_setting) * "-4.3") #isapprox returns true when difference is within x*1e-5
+           string(Int(round(desired_setting))) * "-4.3" : string(desired_setting) * "-4.3") #isapprox returns true when difference is within x*1e-5
         else
 
             #Original code rounded to 1 decimal place, can't figure out why so didn't
             setting_str = (isapprox(desired_setting, round(desired_setting)) ? 
             ("0-" * string(Int(round(desired_setting)))) : ("0-" * string(desired_setting))) #'*' works only if both are strings, but is faster than ',' 
         end
+    else
+        println("Enter a valid setting: $(available_settings)")
     end
 
     cols = Dict(
@@ -96,53 +99,59 @@ function main()
     reb_vel = reb_vel[sorted_index]
     reb_force = reb_force[sorted_index]
 
-    #Compression data points plotted in blue, rebound data points in green
-    lines!(ax, comp_vel, comp_force, color=:blue, label = "$(setting_str) Compression")
-    lines!(ax, reb_vel, reb_force, color=:green, label = "$(setting_str) Rebound")
-
     last_val = round(comp_vel[end], digits=2)
-
-    print("Enter velocity to query (0.0 - $last_val): ")
-    query_velocity = parse(Float64, readline())
 
     #Interpolation used is linear, for higher degrees replace Linear() with Quadratic(), Cubic(), etc.
     #Syntax is interpolation function = interpolate(tuple of x coordinates, array of y-coordinates, Gridded(degree polynomial))
     comp_interp = extrapolate(interpolate((comp_vel,), comp_force, Gridded(Linear())), Line()) #(comp_vel,) makes comp_vel a tuple, needed to pass vertical line test
     reb_interp  = extrapolate(interpolate((reb_vel,), reb_force, Gridded(Linear())), Line()) #Gridded creates irregular grids based on spacing between x-coordinates
 
-    #Calculate and store interpolated points with user's specified velocity
-    comp_query = comp_interp(query_velocity)
-    reb_query = reb_interp(query_velocity)
+    return comp_interp, reb_interp, last_val
+end
 
-    println("At velocity $query_velocity mm/s:")
-    println("  Compression force (N)≈ $comp_query")
-    println("  Rebound force (N)    ≈ $reb_query")
+function display_curves(comp_func, reb_func, max_vel)
+    fig = Figure(size = (1000, 700)) #Inches to pixels conversion using 100 dpi
+    ax = Axis(fig[1, 1], xlabel="Velocity (mm/sec)", ylabel="Force (N)")
 
-    #Scatter! adds points on top of existing axis (plot)
-    scatter!(ax, [query_velocity], [comp_query], color=:yellow, markersize=15, label="Queried Compression")
-    scatter!(ax, [query_velocity], [reb_query], color=:blue, markersize=15, label="Queried Rebound")
-    display(fig)  #shows the figure window
+    #Plot from 0-max_vel (mm/s)
+    comp_x = range(0, max_vel, length=100)
+    reb_x = range(0, max_vel, length=100)
+
+    #Evaluate with given ranges
+    comp_y = comp_func.(comp_x)
+    reb_y = reb_func.(reb_x)
 
 
-    #Loop for user input
-    while true
-        println("Enter velocity (0.0 - $last_val) to query, or 'q' to quit: ")
-        input = readline();
-        if lowercase(input) == "q"
-            break
-        end
-        query_velocity = parse(Float64, input)
-        comp_query = comp_interp(query_velocity)
-        reb_query = reb_interp(query_velocity)
+    #Compression data points plotted in blue, rebound data points in green
+    lines!(ax, comp_x, comp_y, color=:blue, linewidth=2, label="Compression curve")
+    lines!(ax, reb_x, reb_y, color=:red, linewidth=2, label="Rebound curve")
 
-        println("At velocity $query_velocity mm/s:")
-        println("  Compression force (N)≈ $comp_query")
-        println("  Rebound force (N)    ≈ $reb_query")
+    axislegend(ax) #Displays labels
+    display(fig) #Shows the figure window
+end
 
-        #Scatter! adds points on top of existing axis (plot)
-        scatter!(ax, [query_velocity], [comp_query], color=:yellow, markersize=15, label=nothing)
-        scatter!(ax, [query_velocity], [reb_query], color=:blue, markersize=15, label=nothing)
-    end
+function use_funcs(comp_func, reb_func,)
+    return query_vel -> (comp_func(query_vel), reb_func(query_vel)) #Uses a closure which is pretty interesting
+end
+
+function main() 
+    #Returns available settings for specified file in an array
+    println(get_settings("DSD_12_LS.csv")) 
+
+    #Returns interpolation functions for compression and rebound, and also max compression velocity in data set, explicitly cast to Float64 to be safe
+    comp_func, reb_func, max_vel = get_interpolation("DSD_12_LS.csv", Float64(2)) 
+
+    #Plots and displays damper curves given compression and rebound interpolation functions, from 0-max_vel (mm/sec)
+    display_curves(comp_func, reb_func, max_vel)
+
+    #Setup so that only velocity has to be passed into get_force function
+    get_force = use_funcs(comp_func, reb_func)
+
+    #Pass velocity in mm/sec, returns compression and rebound force values in N
+    comp_val, reb_val = get_force(100.5)
+    println("At velocity 100.5 mm/s: Compression ≈ $comp_val, Rebound ≈ $reb_val")
+
+    sleep(10) #Just to show the graph
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
