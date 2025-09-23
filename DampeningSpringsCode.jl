bb06bbd342edf2553770dc44c7b4be7f9dc85bca
SirCrocs <royliu2007@gmail.com>
2025-09-23 12:59:57 -0500

    Added get_func and display_velocity functions

--- DampeningSpringsCode.jl
+++ DampeningSpringsCode.jl
@@ -134,15 +134,81 @@ function use_funcs(comp_func, reb_func,)
     return query_vel -> (comp_func(query_vel), reb_func(query_vel)) #Uses a closure which is pretty interesting
 end
 
+function get_func(desired_settings::Tuple{Float64, Float64}) #Needs LS_setting, HS_setting
+    LS_interpolations = [] #array of interpolation functions for lowspeeds
+    LS_available_settings = get_settings("DSD_12_LS.csv")
+    for i in LS_available_settings
+        push!(LS_interpolations, get_interpolation("DSD_12_LS.csv", Float64(i)))
+    end
+
+    HS_interpolations = [] #array of interpolation functions for highspeeds
+    HS_available_settings = get_settings("DSD_12_HS.csv")
+    for i in HS_available_settings
+        push!(HS_interpolations, get_interpolation("DSD_12_HS.csv", Float64(i)))
+    end
+
+    settings = collect(desired_settings)
+    
+    if settings[1] in LS_available_settings && settings[2] in HS_available_settings
+        if settings[2] == 4.3
+            return LS_interpolations[findfirst(==(settings[1]),LS_available_settings)][1], LS_interpolations[findfirst(==(settings[1]),LS_available_settings)][2], LS_interpolations[findfirst(==(settings[1]),LS_available_settings)][3]
+        elseif settings[1] == 0
+            return HS_interpolations[findfirst(==(settings[2]),HS_available_settings)][1], HS_interpolations[findfirst(==(settings[2]),HS_available_settings)][2], HS_interpolations[findfirst(==(settings[2]),HS_available_settings)][3]
+        else
+            diff_comp_func = x -> HS_interpolations[findfirst(==(settings[2]),HS_available_settings)][1](x) + (LS_interpolations[findfirst(==(settings[1]),LS_available_settings)][1](x) - LS_interpolations[1][1](x))
+            diff_reb_func = x -> HS_interpolations[findfirst(==(settings[2]),HS_available_settings)][2](x) + (LS_interpolations[findfirst(==(settings[1]),LS_available_settings)][2](x) - LS_interpolations[1][2](x))
+            return diff_comp_func, diff_reb_func, HS_interpolations[findfirst(==(settings[2]), HS_available_settings)][3]
+        end
+    else
+        println("Please enter a valid setting: {$LS_available_settings, $HS_available_settings}")
+    end
+end
+
+function display_velocity(vel::Float64)
+    fig = Figure(size = (1000, 700))
+    ax = Axis(fig[1, 1], xlabel="Setting Combination", ylabel="Force (N)")
+
+    LS_available = get_settings("DSD_12_LS.csv")
+    HS_available = get_settings("DSD_12_HS.csv")
+
+    comp_forces = Float64[]
+    reb_forces = Float64[]
+    labels = String[]
+
+    for ls in LS_available
+        for hs in HS_available
+            comp_func, reb_func, _ = get_func((ls, hs))
+            push!(comp_forces, comp_func(vel))
+            push!(reb_forces,  reb_func(vel))
+            push!(labels, "LS=$(ls), HS=$(hs)")
+        end
+    end
+
+    #plot as grouped bars for each setting combination
+    x = 1:length(labels)
+    barplot!(ax, x .- 0.2, comp_forces, color=:blue, width=0.4, label="Compression")
+    barplot!(ax, x .+ 0.2, reb_forces,  color=:red,  width=0.4, label="Rebound")
+
+    ax.xticks = (x, labels)
+    ax.xticklabelrotation[] = π/4 #tilt labels for readability
+    axislegend(ax)
+    display(fig)
+end
+
+
 function main() 
     #Returns available settings for specified file in an array
-    println(get_settings("DSD_12_HS.csv")) 
+    #println(get_settings("DSD_12_HS.csv")) 
 
     #Returns interpolation functions for compression and rebound, and also max compression velocity in data set, explicitly cast to Float64 to be safe
-    comp_func, reb_func, max_vel = get_interpolation("DSD_12_HS.csv", Float64(2)) 
+    #comp_func, reb_func, max_vel = get_interpolation("DSD_12_HS.csv", Float64(2)) 
+
+    comp_func, reb_func, max_vel = get_func((Float64(2),Float64(3))) #Excepts tuple of floats, gets the appropriate interpolation function based on the settings inputted: (LS, HS)
 
     #Plots and displays damper curves given compression and rebound interpolation functions, from 0-max_vel (mm/sec)
     display_curves(comp_func, reb_func, max_vel)
+
+    display_velocity(100.5) #Displays compression and rebound for each possible setting for a specific velocity (mm/s)
 
     #Setup so that only velocity has to be passed into get_force function
     get_force = use_funcs(comp_func, reb_func)
@@ -151,7 +217,7 @@ function main()
     comp_val, reb_val = get_force(100.5)
     println("At velocity 100.5 mm/s: Compression ≈ $comp_val, Rebound ≈ $reb_val")
 
-    sleep(10) #Just to show the graph
+    sleep(20) #Just to show the graph
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
